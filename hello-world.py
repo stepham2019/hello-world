77abfd7e2ef9bb112cc8b573ced13ab9cc17632e
stepham2019 <48837923+stepham2019@users.noreply.github.com>
2020-11-01 15:02:39 +0100

    Update hello-world.py

--- hello-world.py
+++ hello-world.py
@@ -6,4 +6,4 @@ print ("Hello world - development branch")
 
 print ("Zweiter DevBranch")
 
-print ("Änderung") # Noch eine Änderung - Und noch eine
+print ("Änderung") # Noch eine Änderung - Und noch eine - Und die letzte
